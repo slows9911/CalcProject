6e249c8a19830616e0382ca1d09338d88713f8ba
slows9911 <32136949+slows99@users.noreply.github.com>
2020-11-02 00:09:41 -0500

    Feature: Setup for Calculator

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -2,7 +2,13 @@ import unittest
 
 from Calculator import Calculator
 
+
 class MyTestCase(unittest.TestCase):
 
     def test_instantiate_calculator(self):
-        self.assertIsInstance(self.calculator, calculator)
+        calculator = Calculator()
+        self.assertIsInstance(calculator, Calculator)
+
+
+if __name__ == '__main__':
+    unittest.main()
