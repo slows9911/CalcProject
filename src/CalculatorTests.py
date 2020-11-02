4c2cffcfb0db02f7fc2476b998be7e770da977cd
slows9911 <32136949+slows99@users.noreply.github.com>
2020-11-02 03:01:55 -0500

    Feature:  Testn

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -5,8 +5,11 @@ from Calculator import Calculator
 
 class MyTestCase(unittest.TestCase):
 
-    def test_instantiate_calculator(self):
+    def setUp(self) -> None:
         calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        ##calculator = Calculator()
         self.assertIsInstance(calculator, Calculator)
 
     def test_results_property_calculator(self):
