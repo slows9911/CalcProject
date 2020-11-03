99d664677524aa680af056dc8b1bee71ebf4cc1a
slows9911 <32136949+slows99@users.noreply.github.com>
2020-11-02 23:37:29 -0500

    Feature:  Calculator functions added

--- src/Calculator.py
+++ src/Calculator.py
@@ -15,3 +15,37 @@ def squareNumber(d1):
 
 def squareRoot(d1):
     return d1**(1/2.0)
+
+class calculator:
+
+    result = 0
+
+    def __init__(self):
+        self.result = 1
+        pass
+
+    def add(self, d1, d2):
+        self.result = addition(d1, d2)
+        return self.result
+
+    def sub(self, d1, d2):
+        self.result = subtraction(d1, d2)
+        return self.result
+
+    def mult(self, d1, d2):
+        self.result = multiplication(d1, d2)
+        return self.result
+
+    def div(self, d1, d2):
+        self.result = division(d1, d2)
+        self.result = float("{:.3f}".format(self.result))
+        return self.result
+
+    def sqr(self, d1):
+        self.result = square(d1)
+        return self.result
+
+    def sqrt(self, d1):
+        self.result = squareRoot(d1)
+        self.result = float("{:.3f}".format(self.result))
+        return self.result
